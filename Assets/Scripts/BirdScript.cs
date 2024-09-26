41441b37595ee60ad1607d94030b4af453946623
FlyingCow32 <gael.tournier32@gmail.com>
2024-09-26 16:44:59 +0200

    Basic game finished !
    I finished the basic game !
    You can see the tutorial video in the README.md file and others information about my game !

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+
+    public LogicScript logic;
     //public to make it visible, the second is to store it. 
     //to establish a line of communation between the two.
     public Rigidbody2D MyRigidBody;
     // Start is called before the first frame update
     //Runs at the start of the game and runs once.
     public float flapStrength;
+    public bool birdIsAlive = true;
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
@@ -20,11 +23,18 @@ public class BirdScript : MonoBehaviour
     void Update()
     {
         // one = to make the thing the same, two to check if it's the same
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive == true)
         {
             MyRigidBody.velocity = Vector2.up * flapStrength;
             //to get the bird to fly up 
             //test
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameover();
+        birdIsAlive = false;
+    }
+
 }
